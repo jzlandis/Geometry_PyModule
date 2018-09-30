2c158d961b5819d60dd91ec5444dcac1e0e55f21
Jeremy Landis <j.z.landis@hotmail.com>
2018-09-30 14:07:07 -0500

    Change the thing to python3 compatible, wasn't actually before. Implement some more methods to cartesianVector and start on cartesianScalarField.

--- classes.py
+++ classes.py
@@ -4,9 +4,7 @@
 
 from copy import deepcopy
 import numpy as np
-
-def dot(v1,v2):
-	return sum([x*y for x,y in zip(v1,v2)])
+import math
 
 class cartesianBasis():
 	def __init__(self,basisVectors):
@@ -26,14 +24,22 @@ class cartesianBasis():
 			return False
 	def __iter__(self):
 		for i in self.E:
-			return i
+			yield i
 	def __getitem__(self,index):
 		return self.E[index]
 
 class cartesianVector():
 	def __init__(self,components,basis=None):
-		self.comps = [float(x) for x in components]
-		self.basis = deepcopy(basis)
+		if isinstance(components,cartesianVector):
+			if basis == None:
+				self.comps = [float(x) for x in components._toGlobal()]
+				self.basis = None
+			else:
+				self.comps = [float(x) for x in components]
+				self.basis = deepcopy(basis)
+		else:
+			self.comps = [float(x) for x in components]
+			self.basis = deepcopy(basis)
 	def _changeBasis(self,newBasis):
 		if newBasis == self.basis:
 			return self
@@ -62,6 +68,14 @@ class cartesianVector():
 	def cross(self,other):
 		otherChangedBasis = other._changeBasis(self.basis)
 		return cartesianVector([self[i]*otherChangedBasis[j]-self[j]*otherChangedBasis[i] for i,j in ((1,2),(2,0),(0,1))],basis=self.basis)
+	def angle(self,other):
+		return math.acos(self.dot(other._changeBasis(self.basis)) / (self.magnitude()*other.magnitude()))
+	def area(self,other):
+		return 0.5*self.cross(other).magnitude()
+	def volume(self,other1,other2):
+		return abs(other2.dot(self.cross(other1)))/6.
+	def normal(self,other):
+		return cartesianVector(cartesianBasis((self,other))[2])._changeBasis(self.basis)
 	def __add__(self,other):
 		return cartesianVector([x+y for x,y in zip(self,other._changeBasis(self.basis))],basis=self.basis)
 	def __sub__(self,other):
@@ -70,12 +84,39 @@ class cartesianVector():
 		return self.comps[index]
 GLOBAL_CARTESIAN_BASIS = cartesianBasis(((1.,0.,0.),(0.,1.,0.)))
 
+class cartesianScalarField():
+	def __init__(self,funcs,basis=None,origin=(0.,0.,0.)):
+		self.funcs = funcs
+	def __call__(self,position):
+		return sum([x(position[i]) for i,x in enumerate(self.funcs)])
+
+
 yAxisBasis = cartesianBasis([[0.,1.,0.],[0.,0.,1.]])
 yAxisVector = cartesianVector([1,0,0],basis=yAxisBasis)
 xAxisBasis = cartesianBasis([[1,0,0],[0,1,0]])
 xAxisVector = cartesianVector([1,0,0],basis=xAxisBasis)
+zAxisBasis = cartesianBasis([[0.,0.,1.],[1.,0.,0.]])
+zAxisVector = cartesianVector([1,0,0],basis=zAxisBasis)
 
-print "Should be a global z:"
-print xAxisVector.cross(yAxisVector)
-print "Should be zero:"
-print xAxisVector.dot(yAxisVector)
+def funca(x):
+	return x**0.2
+def funcb(x):
+	return x/4.
+def funcc(x):
+	return x+2
+
+print("Should be a global z:")
+print(xAxisVector.cross(yAxisVector))
+print("Should be zero:")
+print(xAxisVector.dot(yAxisVector))
+print("Should be 90 degrees (1.57 rad):")
+print(xAxisVector.angle(yAxisVector))
+print("Should be 0.5 (triangular area between yAxisVector and yAxisVector)")
+print(xAxisVector.area(yAxisVector))
+print("Should be 1/6 (0.1667) (tetrahedron volume between x,y,zAxisVectors)")
+print(xAxisVector.volume(yAxisVector,zAxisVector))
+print("Should be a global z ")
+print(yAxisVector.normal(xAxisVector)._toGlobal())
+
+cSFex = cartesianScalarField((funca,funcb,funcc))
+print(cSFex((1,1,1)))
